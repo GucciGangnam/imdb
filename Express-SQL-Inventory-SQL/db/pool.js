4c7871064d44ee7cacda6137f95134a329b0b38b
Gucci Gangnam <diviner02hive@icloud.com>
2024-08-11 16:24:39 +0700

    Hosted DB pool env links added

--- Express-SQL-Inventory-SQL/db/pool.js
+++ Express-SQL-Inventory-SQL/db/pool.js
@@ -1,11 +1,32 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
+// HOSTED DB ENV FILES
 module.exports = new Pool({
-    host: process.env.POOL_HOST, 
-    user: process.env.POOL_USER,
+    host: process.env.POOL_HOST,
+    port: process.env.POOL_PORT,
     database: process.env.POOL_DB,
+    user: process.env.POOL_USER,
     password: process.env.POOL_PASS,
-    port: 5432 // The default port
+    ssl: {
+        rejectUnauthorized: false // Adjust this based on your SSL requirements
+    }
 });
+
+
+
+
+
+
+// LOCAL DB ENV FILES
+
+// const { Pool } = require("pg");
+
+// // All of the following properties should be read from environment variables
+// // We're hardcoding them here for simplicity
+// module.exports = new Pool({
+//     host: process.env.POOL_HOST, 
+//     user: process.env.POOL_USER,
+//     database: process.env.POOL_DB,
+//     password: process.env.POOL_PASS,
+//     port: 5432 // The default port
+// });
